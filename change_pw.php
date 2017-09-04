0318fec6a84b9b7b64ff28aa0ad2fdf3ac98b5eb
crforest0301 <crforest0301@gmail.com>
2017-09-04 19:01:05 +0900

    Add two functions (Project finished)
    'Change administrator password' form and logic
    'Logout' logic
    
    *This project has been finished today.
    I'll add more function and debug errors undiscovered when I have ideas
    about them.

--- change_pw.php
+++ change_pw.php
@@ -1,3 +1,19 @@
+<!--
+*조건문
+    관리자 로그인했다면
+            폼에 값이 제대로 입력됐다면
+                    새 비번과 새 비번 재입력이 다르다면
+                    새 비번이 16자리보다 크거나, 8자리보다 작다면
+                    아니라면
+                            SQL이 정상 수행되지 않는다면
+                            아니라면
+            아니라면
+
+    아니라면
+
+*기존 비밀번호 일치여부 체크 로직은 없다. 추가 필요 -!
+-->
+
 <?php
 
 session_start();
@@ -7,7 +23,7 @@ require_once './header.php';
 
 echo "<h3>비밀번호 변경</h3>";
 
-if(check_admin()){
+if(check_admin()){ //*1
     if(filled_out($_POST)){
         $username=$_SESSION['admin_id'];
         $new_pw=$_POST['new_pw'];
@@ -17,10 +33,20 @@ if(check_admin()){
         if($new_pw !=$renew_pw){
             echo "<p align='center'>새로운 비밀번호를 재확인하시기 바랍니다.</p>";
             echo "<p align='center'><a href='change_pw_form.php'>다시 입력하기</a></p>";   
-        }else if(){
-            
+        }else if((strlen($new_pw)>16)||(strlen($new_pw)<8)){
+            echo "<p align='center'>비밀번호는 8~16자리입니다. 다시 확인 바랍니다.</p>";
+            echo "<p align='center'><a href='change_pw_form.php'>다시 입력하기</a></p>";   
+
         }else{
+            $db=db_conn();
+            $sql="update admin set password=sha1('".$new_pw."') where username='".$username."'";
+            $result=$db->query($sql);
             
+            if(!$result){
+                echo "<p align='center'>비밀번호 변경 처리 실패.</p>";
+            }else{
+                echo "<p align='center'>비밀번호가 성공적으로 변경 처리되었습니다.</p>";                
+            }
         }
     }else{
         ?>
@@ -36,5 +62,6 @@ if(check_admin()){
         echo "<p align='center'><a href='adminLogin.php'>관리자 로그인</a></p>";    
 }
 
+echo "<hr/><p align='center'><a href='admin.php'>관리자 메뉴</a></p>";  
 require_once './footer.php';
 ?>
